fe64d15b970d3de99a3d4b822903d8189332fc95
ColbyMayDesign <47373853+JobinC@users.noreply.github.com>
2021-03-18 12:29:52 -0700

    dns-queries is logged as json data, p5 is able to read this format in the noise-circle example

--- noise-circle/sketch.js
+++ noise-circle/sketch.js
@@ -1,35 +1,37 @@
 var t;
 
 function setup() {
-  createCanvas(400, 400);
-  background(255);
-  stroke(0, 15);
-  noFill();
-  t = 0;
-  frameRate(2);
+    createCanvas(1000, 1000);
+    background(255);
+    stroke(0, 15);
+    noFill();
+    t = 0;
+    frameRate(25);
+    let JSON = ('../pi-hole-data.json');
+    pi_data = loadJSON(JSON);
 }
 
 function draw() {
     queries = loadStrings('../pi-hole-data.txt')
-print(queries);
-t = queries;
-  translate(width/2, height/2);
-  beginShape();
-  for (var i = 0; i < 200; i++) {
-    var ang = map(i, 0, 200, 0, PI);
-    var rad = 200 * noise(i * 0.01, t * 0.005);
-    var x = rad * cos(ang);
-    var y = rad * sin(ang);
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
+        // print(queries);
+    let queries_blocked = pi_data.dns_queries_today;
+    print(queries_blocked);
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < queries_blocked; i++) {
+        var ang = map(i, 0, 200, 0, PI);
+        var rad = 400 * noise(i * 0.01, t * 0.005);
+        var x = rad * cos(ang);
+        var y = rad * sin(ang);
+        curveVertex(x, y);
+    }
+    endShape(CLOSE);
 
-  t += 1;
+    t += 1;
 
-//   // clear the background every 600 frames using mod (%) operator
-//   if (frameCount % 600 == 0) {
-// 	background(255);
-//   }
+    //   // clear the background every 600 frames using mod (%) operator
+    //   if (frameCount % 600 == 0) {
+    // 	background(255);
+    //   }
 
 }
-
